8071d5a2c7df77f3506d84720165ff96df888e28
Sofia Dubrowsky <sofidubrowsky@hotmail.com.ar>
2023-08-03 01:32:57 -0300

    landing css

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -6,19 +6,19 @@ import { NavLink } from "react-router-dom";
 
 
 
-const Landing = ()=>{
-    return(
-        <div className={style.landing}>
-            <div className={style.shadow}>
-            <div className={style.topLeft} >
-            <a
+const Landing = () => {
+  return (
+    <div className={style.landing}>
+      <div className={style.shadow}>
+        <div className={style.topLeft} >
+          <a
             href="https://www.linkedin.com/in/disof%C3%ADadubrowsky/"
             target="_blank"
             rel="noreferrer"
             className={style.alink}
           >
             <img src={linkedin} alt="linkedin" className={style.img} />
-          </a> 
+          </a>
           <a
             href="https://github.com/SofiDubrowsky"
             target="_blank"
@@ -26,17 +26,26 @@ const Landing = ()=>{
             className={style.alink}
           >
             <img src={github} alt="github" className={style.img} />
-          </a> 
-            </div>
-
-            <div className={style.text}> 
-             <h5 className={style.textsmall}>Henry PI</h5>
-             <h1>Countries App Proyect</h1>
-             <NavLink to="/home"> <img className={style.rosa} src={rosadelosvientos} alt="start" /> </NavLink>
-             
-            </div>
-            </div>
+          </a>
         </div>
-    )
+
+        <div className={style.text}>
+
+          <div className={style.textsmall}>
+            <h5>Henry PI</h5>
+          </div>
+
+          <div className={style.textBig}>
+            <h1>Countries App Proyect</h1>
+          </div>
+
+          <div className={style.imagen}>
+            <NavLink to="/home"> <img className={style.rosa} src={rosadelosvientos} alt="start" /> </NavLink>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Landing;
